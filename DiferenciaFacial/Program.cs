f6035de0977f21027ce9e707732342195f978bd8
Gino Imanol <ginoimanol25@gmail.com>
2023-11-08 10:42:14 -0300

    aplicacion de consola para deteccion facil y separacion de video en frames con opencv

---
+++ DiferenciaFacial/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using System.IO;
+using OpenCvSharp.Dnn;
+using OpenCvSharp.Extensions;
+
+
+namespace DiferenciaFacial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directorioActual = Directory.GetCurrentDirectory();
+
+            string rutaVideo = Path.Combine(directorioActual, "videoprueba.mp4");                           //Creacion de rutas que nos servirán a lo largo del programa
+            string rutaFrames = Path.Combine(directorioActual, "framesFaciales");
+            string rutaHaarcascade = Path.Combine(directorioActual, "haarcascade_frontalface_default.xml");
+
+            CascadeClassifier faceCascade = new CascadeClassifier(rutaHaarcascade);                     //crea un objeto CascadeClassifier para la detección de rostros.
+
+            VideoCapture capture = new VideoCapture(rutaVideo);
+            Mat Frame = new Mat();
+            {
+                int frameNumber = 0;                                //Inicializamos el nro del frame en 0, para luego nombrar los frames.
+
+                while (capture.Read(Frame))                          //cada frame que tome del video lo guarda en "frame" mientras capture tenga frames.
+                {
+                    Mat grayImage = new Mat();                      
+                    Cv2.CvtColor(Frame, grayImage, ColorConversionCodes.BGR2GRAY);      //Se crea una imagen en escala de grises a partir del frame  "Frame".
+
+                    Rect[] faces = faceCascade.DetectMultiScale(                      //Detecta rostros en la imagen en escala de grises  
+                        grayImage,
+                        scaleFactor: 1.1,
+                        minNeighbors: 5,
+                        flags: HaarDetectionTypes.DoRoughSearch,
+                        minSize: new Size(30,30)
+                        );
+                
+                    foreach(Rect face in faces)
+                    {
+                        Cv2.Rectangle(Frame, face, Scalar.Red, 2); //En esta parte se dibuja los rectangulos al rededor de los rostros
+                    }
+                    string frameFileName = $"{rutaFrames}\\frame_{frameNumber:D4}.jpg"; //Se crea el string que será el nombre de cada frame y le damos la dirección de la carpeta en la que vamos a guardar los frames.
+                    Cv2.ImWrite(frameFileName, Frame);                  //teniendo nombre del frame y el frame, se guarda en la carpeta "frames" en formato .jpg
+                    frameNumber++;
+                }
+
+
+            }
+        }
+    }
+}
---
+++ PruebaOpenCv/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using System.IO;
+
+namespace PruebaOpenCv
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string directorioActual = Directory.GetCurrentDirectory();  //Se crea un string con la ruta actual para luego poder usar una ruta relativa. Crear la ruta relativa nos va a permitir ejecutar correctamente la aplicacion sin necesitar la ruta absoluta.
+
+            string rutaVideo = Path.Combine(directorioActual, "cuenta.mp4"); //Se utiliza la ruta "directorioActual" para crear la ruta relativa del video "cuenta.mp4"
+            string rutaFrames = Path.Combine(directorioActual, "frames"); //Se utiliza la ruta "directorioActual" para crear la ruta relativa de la carpeta "frames"
+
+            VideoCapture capture = new VideoCapture(rutaVideo); //Creamos el objeto capture con el video "cuenta.mp4", utilizando la ruta "rutaVideo"
+            Mat frame = new Mat();                                  //Creamos el objeto frame vacio.
+            {
+                int frameNumber = 0;                                //Inicializamos el nro del frame en 0, para luego nombrar los frames.
+                
+                while(capture.Read(frame))                          //cada frame que tome del video lo guarda en "frame" mientras capture tenga frames.
+                {
+                    string frameFileName = $"{rutaFrames}\\frame_{frameNumber:D4}.jpg"; //Se crea el string que será el nombre de cada frame y le damos la dirección de la carpeta en la que vamos a guardar los frames.
+                    Cv2.ImWrite(frameFileName, frame);                  //teniendo nombre del frame y el frame, se guarda en la carpeta "frames" en formato .jpg
+                    frameNumber++;                                       //incrementamos en 1 el numero de frame "frameNumber"
+
+                
+                }
+                capture.Dispose();                              //Liberamos los recursos del objeto capture
+            }
+        }
+    }
+}
